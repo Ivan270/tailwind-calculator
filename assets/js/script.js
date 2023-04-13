62151dd34edb8c182716f29bdd63e0427fa537bd
Ivan Verdugo <ivan270@gmail.com>
2023-04-12 20:42:01 -0400

    project completed

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+let teclado = document.querySelector('.calculadora__teclado');
+// let botones = document.querySelectorAll('button');
+let pantalla = document.getElementById('pantalla');
+pantalla.innerText = '';
+teclado.addEventListener('click', (e) => {
+	let boton = e.target;
+	let tecla = boton.innerText;
+	// Si es que boton presionado tiene la clase '.numeros'...
+	if (boton.matches('.numeros')) {
+		pantalla.innerText += tecla;
+	} else if (boton.matches('.operadores')) {
+		if (boton.innerText == '=') {
+			let resultado = eval(pantalla.innerText);
+			console.log(resultado);
+			pantalla.innerText = resultado;
+		} else if (boton.innerText == 'AC') {
+			pantalla.innerText = '';
+		} else {
+			pantalla.innerText += tecla;
+		}
+	}
+});
